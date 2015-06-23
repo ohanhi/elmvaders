5de32e23b8b081248aa00bd779cbffd6b830c545
ohanhi <ossi.hanhinen@gmail.com>
2015-06-23 22:56:39 +0300

    Add dotProduct and Rectangle+overlap

--- Util.elm
+++ Util.elm
@@ -20,3 +20,23 @@ vecAdd a b =
 vecSubtract : Vec2 -> Vec2 -> Vec2
 vecSubtract a b =
   vecAdd a (vecScalarMul b -1)
+
+dotProduct : Vec2 -> Vec2 -> Vec2
+dotProduct a b =
+  { x = a.x * b.x, y = a.y * b.y }
+
+-- rectangle in 2D space
+type alias Rectangle = { center : Vec2, size : Vec2 }
+
+{--
+Two rectangles overlap if the distance between the central points
+along the x and y axes are less than their sizes halved.
+--}
+overlap : Rectangle -> Rectangle -> Bool
+overlap a b =
+  let sub       = vecSubtract a.center b.center
+      aSizeHalf = vecScalarMul a.size 0.5
+      bSizeHalf = vecScalarMul b.size 0.5
+      maxDx     = aSizeHalf.x + bSizeHalf.x
+      maxDy     = aSizeHalf.y + bSizeHalf.y
+  in  abs sub.x < maxDx && abs sub.y < maxDy
