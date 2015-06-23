b4bb7ace806f46c5a1a7b84d480eaef8e0607f89
ohanhi <ossi.hanhinen@gmail.com>
2015-06-23 22:14:37 +0300

    Add vecAdd and vecSubtract

--- Util.elm
+++ Util.elm
@@ -12,3 +12,11 @@ zeroVec2 = { x = 0, y = 0 }
 vecScalarMul : Vec2 -> Float -> Vec2
 vecScalarMul v c =
   { x = v.x * c, y = v.y * c }
+
+vecAdd : Vec2 -> Vec2 -> Vec2
+vecAdd a b =
+  { x = a.x + b.x, y = a.y + b.y }
+
+vecSubtract : Vec2 -> Vec2 -> Vec2
+vecSubtract a b =
+  vecAdd a (vecScalarMul b -1)
